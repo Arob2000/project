37e1f3cc6b4bd06e48f8c249ed38e35f0d429a39
Arob2000 <robafnan8@gmail.com>
2025-08-03 12:42:59 +0300

    Added new landing page updates

--- src/landingpage.js
+++ src/landingpage.js
@@ -1,11 +1,37 @@
-import React, { useState } from 'react';
+import React from "react";
+import "./landingpage.css";       
+import landingphoto from "./image/landingphoto.png"; 
 
 function LandingPage({ startDemo }) {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>This is React Project</h1>
-      <p>Welcome to my course react.</p>
-      <button onClick={startDemo}>Start Demo</button>
+    <div
+      className="landing-page"
+      style={{ backgroundImage: `url(${landingphoto})` }}
+    >
+      {}
+      <nav className="navbar">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+
+      {}
+      <div className="top-text">
+        <h1 className="logo">WEB PROJECT</h1>
+        <p className="subtext">fronted</p>
+      </div>
+
+      {}
+      <div className="bottom-bar">
+        Hellow From React & JavaScript Course Learning
+      </div>
+
+      {}
+      <button className="second-page" onClick={startDemo}>
+        Next page
+      </button>
     </div>
   );
 }
